7a44d3d976330774db47af3dcfc43501fccddca2
Julian Gruber <julian@juliangruber.com>
2014-02-13 12:04:22 +0100

    initial commit

---
+++ diff.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash -e
+
+# arguments
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: npm-diff <module> <versionA> <versionB>"
+  exit
+fi
+
+module=$1
+versionA=$2
+versionB=$3
+
+# colored diff with fallback
+
+cdiff() {
+  if hash colordiff 2>/dev/null; then
+    colordiff "$@"
+  else
+    diff "$@"
+  fi
+}
+
+# work dirs
+
+a=/tmp/$RANDOM
+b=/tmp/$RANDOM
+mkdir $a $b
+
+# checkout
+
+(cd $a; npm install $module@$versionA) &
+(cd $b; npm install $module@$versionB) &
+wait
+
+# diff
+
+cdiff -u -x package.json -x component.json $a/node_modules/$module $b/node_modules/$module
+
+# cleanup
+
+rm -Rf $a $b
+
